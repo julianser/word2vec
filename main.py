7b83a7bd359845406d20b50715c679444c3810cb
Michael Noukhovitch <mnoukhov@uwaterloo.ca>
2016-09-22 16:13:10 -0400

    main and model kinda done

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import time
+
 import theano
 from theano import tensor as T
 
+from word2vec import Word2Vec
+
 def main():
     data = load_data()
-    data_train, data_eval, data_test = data
+    query_train, context_train, query_eval, context_eval = data
+
+
+
+    for epoch in range(num_epochs):
+        train_err = 0
+        train_batch = 0
+        start_time = time.time()
+
+        for batch in minibatches:
+            inputs, targets = batch
+            train_err +=
+
 

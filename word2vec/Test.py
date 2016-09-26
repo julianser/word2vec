1e0c35d57c76e5cdeed19446230184e897b01087
Athul Paul Jacob <jacob@pumpup.co>
2016-09-26 14:13:41 -0400

    Added sample file

--- word2vec/Test.py
+++ word2vec/Test.py
@@ -1,184 +1,63 @@
-'''
-The TokenMap provides a mapping from tokens (plain words) to integer ids,
-as well as a map from ids back to tokens.
-'''
-
-import gzip
+from DatasetReader import DatasetReader, default_parse
+from Minibatcher import Minibatcher
 
 
-SILENT = 0
-WARN = 1
-ERROR = 2
-UNK = 0
+def main():
 
+	#Create the dataset reader object
+	#Pretty much the same as the previous implementation
+	reader = DatasetReader(
+		files=files,
+		directories=directories,
+		skip=skip,
+		macrobatch_size=macrobatch_size,
+		max_queue_size=max_queue_size,
+		num_processes=num_processes,
+		unigram_dictionary=unigram_dictionary,
+		load_dictionary_dir=load_dictionary_dir,
+		min_frequency=min_frequency,
+		t=t,
+		kernel=kernel,
+		parse=default_parse,
+		verbose=verbose
+	)
 
-class TokenMap(object):
+	#Create the minibatcher object
+	minibatcher = Minibatcher(
+		batch_size=batch_size,
+		dtype="int32",
+		num_dims=??
+	)
 
-	def __init__(self, on_unk=WARN, tokens=None):
-		'''
-		Create a new TokenMap.  Most common usage is to call this without
-		any arguments.
+	#TODO: Add the loss function
 
-		on_unk:	Controls the behavior when asked to provide the token_id
-			for a token not found in the map.  Default is WARN, which
-			means returning 0 (which is id reserved for unknown tokens)
-			and then printing a warning to stout.  Choose from SILENT,
-			WARN, or ERROR.
+	#Create the SGD update function
+	updates = nesterov_momentum(
+		loss, embedder.get_params(), learning_rate, momentum
+	)
 
-		tokens:	List of strings corresponding to a map that should be
-			used.  The index of a token in the list is used as its ID.
-			Not normally used, because TokenMap provides functions to
-			build the map easily from a corpus.  The first element in the
-			list should be 'UNK', becuase id 0 is reserved for unknown
-			tokens.  Not doing so is an error.
-		'''
+	#Add the minibatcher update function
+	updates.update(minibatcher.get_updates())
 
-		# Validate on_unk
-		if on_unk not in (SILENT, WARN, ERROR):
-			raise ValueError(
-				'on_unk must be one of token_map.SILENT, token_map.WARN, '
-				'or token_map.ERROR.'
-			)
-		self.on_unk = on_unk
+	#Create the training function
+	train = function([], loss, updates=updates)
 
-		# Initialize the token mapping
-		if tokens is None:
-			self.map = {'UNK':UNK}	# keys are tokens, values are ids
-			self.tokens = ['UNK']	# entries are tokens, indices are ids
+	#Train
+	for epoch in range(num_epochs):
 
-		# If an initial lexicon was provided, build the map from it
+		#Load macrobatch from the dataset reader
+		if read_data_async:
+			macrobatches = reader.generate_dataset_parallel()
 		else:
-			if tokens[0] != 'UNK':
-				raise ValueError(
-					'tokens[0] must be "UNK" because ID 0 is reserved for '
-					'unknown tokens.'
-				)
+			macrobatches = reader.generate_dataset_serial()
 
-			self.map = dict((t, idx) for idx, t in enumerate(tokens))
-			self.tokens = tokens
+		macrobatch_num = 0
+		for macrobatch in macrobatches:
 
+			macrobatch_num += 1
 
-	def compact(self):
-		'''
-		Recreate the tokens list and mapping such that `None`s are
-		removed (which are holes left by calls to `remove()`.
-		'''
-		self.tokens = [t for t in self.tokens if t is not None]
-		self.map = dict((t, idx) for idx, t in enumerate(self.tokens))
-
-
-	def remove(self, token):
-		'''
-		Removes token from the TokenMap
-		:param token:
-		'''
-		idx = self.get_id(token)
-		if idx == UNK:
-			raise ValueError(
-				'Cannot remove token %s because it does not '
-				'exist or is reserved.' % str(token)
-			)
-		self.tokens[idx] = None
-
-
-	def add(self, token):
-		'''
-		Adds token to the TokenMap
-		:param token:
-		:return:
-		'''
-		try:
-			return self.map[token]
-		except KeyError:
-			next_id = len(self.tokens)
-			self.map[token] = next_id
-			self.tokens.append(token)
-			return next_id
-
-	def get_vocab_size(self):
-		'''
-		Gets TokenMap size
-		:return:
-		'''
-		return len(self.tokens)
-
-
-	def update(self, token_iterable):
-		'''
-		Adds multiple tokens from token_iterable
-		'''
-		return [self.add(token) for token in token_iterable]
-
-
-	def get_id(self, token):
-		'''
-		Gets the id of token
-		'''
-		try:
-			return self.map[token]
-
-		# If that token isn't in the vocabulary, what should we do?
-		# This depends on the setting for on_unk.  We can return the UNK
-		# token silently, return the UNK token with a warning, or
-		# raise an error.
-		except KeyError:
-			if self.on_unk == SILENT:
-				return self.map['UNK']  # i.e. return 0
-			elif self.on_unk == WARN:
-				print 'Warning, unrecognized token: %s' % token
-				return self.map['UNK']  # i.e. return 0
-			elif self.on_unk == ERROR:
-				raise
-			else:
-				raise ValueError(
-					'Unrecognized value for on_unk in TokenMap.'
-				)
-
-
-	def get_ids(self, token_iterable):
-		'''
-		Gets multiple ids
-		'''
-		return [self.get_id(token) for token in token_iterable]
-
-
-	def get_token(self, idx):
-		'''
-		Gets token with idx
-		'''
-		return self.tokens[idx]
-
-	def get_tokens(self, idx_iterable):
-		'''
-		'''
-		return [self.tokens[idx] for idx in idx_iterable]
-
-
-	def __len__(self):
-		return len(self.tokens)
-
-
-	def save(self, filename):
-		if filename.endswith('.gz'):
-			f = gzip.open(filename, 'w')
-		else:
-			f = open(filename, 'w')
-
-		for idx, token in enumerate(self.tokens):
-			f.write(token + '\n')
-
-
-	def load(self, filename):
-		self.map = {}
-		self.tokens = []
-
-		if filename.endswith('.gz'):
-			f = gzip.open(filename)
-		else:
-			f = gzip.open(filename)
-
-		for idx, line in enumerate(f):
-			token = line.strip()
-			self.map[token] = idx
-			self.tokens.append(token)
-
+			#Insert macrobatch into the minibatcher
+			minibatcher.load_dataset(macrobatch)
+			losses = []
+			for batch_num in range(minibatcher.get_num_batches()):
+				losses.append(train())

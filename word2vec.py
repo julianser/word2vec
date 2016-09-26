7f2bc3f5c4da6fe9eed1809a10eda8fdbbfe6fdd
Michael Noukhovitch <mnoukhov@uwaterloo.ca>
2016-09-26 14:26:28 -0400

    intermediate

--- word2vec.py
+++ word2vec.py
@@ -1,40 +1,41 @@
+from abc import ABCMeta
+
 import theano
 import lasagne
 from theano import tensor as T
 from lasagne import layers as L
 
 
-class Word2Vec(object):
-    def __init__(self, batch_size, query_vocab_size, context_vocab_size,
+class Word2VecBase:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, batch_size, query_input, query_vocab_size, context_vocab_size,
                  emb_dim_size):
         """
         initialize the train and embed methods
         """
-        query_input = T.ivector('query')
-        context_output = T.ivector('context')
-        learning_rate = T.scalar('learning_rate')
-        momentum = T.scalar('momentum')
-
-        embedding, network = self.model(batch_size,
+        self.embedding, self.network = self.model(batch_size,
                                         query_input,
                                         query_vocab_size,
                                         context_vocab_size,
                                         emb_dim_size)
 
-        prediction = L.get_output(network)
-        loss = self.loss(prediction, context_output)
-        params = L.get_all_params(network, trainable=True)
-        updates = lasagne.updates.nesterov_movement(loss, params,
-                                                    learning_rate=learning_rate,
-                                                    momentum=momentum)
-        self.train = theano.function(
-            [query_input, context_output, learning_rate, momentum],
-            loss, updates=updates)
-
         embedding = L.get_output(embedding)
-        self.embed = theano.function([query_input], embedding)
+        self.embed = theano.function([self.query_input], self.embedding)
+
+    @abstractmethod
+    def model(self, batch_size, query_input, query_vocab_size,
+              context_vocab_size, emb_dim_size):
+        raise NotImplementedError
+
+    def get_output(self):
+        return L.get_output(self.network)
+
+    def get_all_params(self):
+        L.get_all_params(self.network, trainable=True)
 
 
+class Word2VecNormal(Word2VecBase):
     def model(self, batch_size, query_input, query_vocab_size,
               context_vocab_size, emb_dim_size):
         l_input = L.InputLayer(shape=(batch_size,),
@@ -47,5 +48,18 @@ class Word2Vec(object):
                              nonlinearity=lasagne.nonlinearities.softmax)
         return l_embed, l_out
 
-    def loss(self, logits, context):
-        return lasagne.objectives.binary_crossentropy(logits, context)
+
+class Word2VecDiscreteContinuous(Word2VecBase):
+    def model(self, batch_size, query_input, query_vocab_size,
+              context_vocab_size, emb_dim_size):
+        l_input = L.InputLayer(shape=(batch_size,),
+                               input_var=query_input)
+        l_embed_continuous = L.EmbeddingLayer(l_input,
+                                   input_size=query_vocab_size,
+                                   output_size=emb_dim_size)
+        l
+        l_out = L.DenseLayer(l_embed,
+                             num_units=context_vocab_size,
+                             nonlinearity=lasagne.nonlinearities.softmax)
+        return l_embed, l_out
+

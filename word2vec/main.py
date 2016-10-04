7e58a89ea5dc5889c39c219e7a57e7c49f0c4ed7
Michael Noukhovitch <mnoukhov@uwaterloo.ca>
2016-10-04 17:58:32 -0400

    basic example not working

--- word2vec/main.py
+++ word2vec/main.py
@@ -13,13 +13,13 @@ from minibatcher import Minibatcher
 
 # theano.config.compute_test_value = 'warn'
 
-def main(files, batch_size, emb_dim_size, learning_rate=0.1, momentum=0.9, num_epochs=3, save_dir=None):
+def train(files, batch_size, emb_dim_size, learning_rate=0.1, momentum=0.9, num_epochs=3, save_dir=None):
     reader = DatasetReader(
         files=files,
         macrobatch_size=10000,
         num_processes=3,
         min_frequency=10,
-        verbose=True)
+        verbose=False)
 
     if not reader.is_prepared():
         reader.prepare()
@@ -35,57 +35,64 @@ def main(files, batch_size, emb_dim_size, learning_rate=0.1, momentum=0.9, num_e
     query_input = batch_rows[:,0]
     context_target = batch_rows[:,1]
 
-    word2vec = Word2VecNormal(batch_size,
-                              query_input=query_input,
-                              context_vocab_size=reader.get_vocab_size(),
-                              query_vocab_size=reader.get_vocab_size(),
-                              emb_dim_size=emb_dim_size)
+    # word2vec = Word2VecNormal(batch_size,
+                              # query_input=query_input,
+                              # context_vocab_size=reader.get_vocab_size(),
+                              # query_vocab_size=reader.get_vocab_size(),
+                              # emb_dim_size=emb_dim_size)
 
-    prediction = word2vec.get_output()
-    loss = categorical_crossentropy(prediction,
-                                    context_target)
-    loss = loss.mean()
-    params = word2vec.get_all_params()
+    # prediction = word2vec.get_output()
+    # loss = categorical_crossentropy(prediction,
+                                    # context_target)
+    # loss = loss.mean()
+    # params = word2vec.get_all_params()
 
-    updates = nesterov_momentum(loss,
-                                params,
-                                learning_rate,
-                                momentum)
-    updates.update(minibatcher.get_updates())
+    # updates = nesterov_momentum(loss,
+                                # params,
+                                # learning_rate,
+                                # momentum)
+    # updates.update(minibatcher.get_updates())
 
-    # train = theano.function([query_input, context_target], loss,
+    train = theano.function([], query_input, updates=minibatcher.get_updates())
+    # train = theano.function([], loss,
                             # updates=updates, mode='DebugMode')
-    train = theano.function([], loss,
-                            updates=updates, mode='DebugMode')
 
     for epoch in range(num_epochs):
         #batches = reader.generate_dataset_parallel()
         batches = reader.generate_dataset_serial()
         for batch_num, batch in enumerate(batches):
+            print 'Hello'
             print 'running batch {}'.format(batch_num)
             minibatcher.load_dataset(batch)
             losses = []
             for minibatch_num in range(minibatcher.get_num_batches()):
                 print 'running minibatch', batch_num
-                losses.append(train())
+                query = train()
+                print 'query {}   context {}'.format(query, None)
 
             print('batch {} Mean Loss {}'.format(batch_num,np.mean(losses)))
 
-    if save_dir:
-        word2vec.save_embedder(save_dir)
+    # if save_dir:
+        # word2vec.save_embedder(save_dir)
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Word2Vec')
-    parser.add_argument('file')
-    parser.add_argument('--batch_size', type=int, default=500)
+    parser.add_argument('mode', choices=['train', 'test'])
+    parser.add_argument('--file', help='file to train off of')
+    parser.add_argument('--batch_size', type=int, default=500, help='size of each training batch')
     parser.add_argument('--embed_size', type=int, default=1000, help='size of the embedding dimension')
-    parser.add_argument('--save_dir')
+    parser.add_argument('--save_dir', help='directory where dictionary + embedder are saved to/loaded from')
 
     args = parser.parse_args()
 
-    main([args.file], args.batch_size, args.embed_size, save_dir=args.save_dir)
+    if args.mode == 'train' and not args.file:
+        raise Exception('Must specify training file if in train mode')
+    elif args.mode == 'test' and not args.save_dir:
+        raise Exception('Must specify directory to load dictionary + embedder from for testing')
+
+    train([args.file], args.batch_size, args.embed_size, save_dir=args.save_dir)
 
 
 
